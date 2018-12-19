d5d3f4d25bd12b617062a07fea2390e80a6a8e1d
max197a <max197aa@gmail.com>
2018-12-20 00:53:16 +0200

    add project

---
+++ gulp/tasks/copy.js
@@ -0,0 +1,32 @@
+var gulp = require("gulp");
+var config = require("../config");
+
+gulp.task("copy:fonts", function() {
+  return gulp
+    .src(config.src.fonts + "/**/*.{eot,ttf,svg,woff,woff2}")
+    .pipe(gulp.dest(config.dest.fonts));
+});
+
+gulp.task("copy:vendor", function() {
+  return gulp
+    .src(config.src.vendor + "/**/*.*")
+    .pipe(gulp.dest(config.dest.vendor));
+});
+
+// gulp.task("copy:rootfiles", function() {
+//   return gulp.src(config.src.root + "/*.*").pipe(gulp.dest(config.dest.root));
+// });
+
+gulp.task("copy", [
+  // 'copy:rootfiles',
+  "copy:vendor",
+  "copy:fonts"
+]);
+
+gulp.task("copy:watch", function() {
+  gulp.watch(config.src.fonts + "/**/*.{eot,ttf,svg,woff,woff2}", [
+    "copy:fonts"
+  ]);
+  gulp.watch(config.src.vendor + "/**/*.*", ["copy:vendor"]);
+  // gulp.watch(config.src.root + "/*.*", ["copy:rootfiles"]);
+});
---
+++ src/js/main.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+  //////////
+  // Global variables
+  //////////
+
+  var _window = $(window);
+  var _document = $(document);
+
+  ////////////
+  // READY - triggered when PJAX DONE
+  ////////////
+
+  // single time initialization
+  // legacySupport();
+  initaos();
+  var easingSwing = [0.02, 0.01, 0.47, 1];
+
+  function pageReady() {
+    // initSmoothScroll();
+  }
+
+  // this is a master function which should have all functionality
+  pageReady();
+
+  //////////
+  // COMMON
+  //////////
+
+  function initaos() {
+    AOS.init();
+  }
+
+  // header scroll
+  _window.on(
+    "scroll",
+    throttle(function() {
+      var scroll = _window.scrollTop();
+      var headerHeight = $(".header").height();
+      var heroHeight = $(".firstscreen").height();
+
+      if (scroll > headerHeight) {
+        $(".header").addClass("is-fixed-start");
+      } else {
+        $(".header").removeClass("is-fixed-start");
+      }
+      if (scroll >= heroHeight - headerHeight / 1) {
+        $(".header").addClass("is-fixed");
+        $(".back-to-top").addClass("is-active");
+      } else {
+        $(".header").removeClass("is-fixed");
+        $(".back-to-top").removeClass("is-active");
+      }
+    }, 25)
+  );
+
+  // Prevent # behavior
+  _document
+    .on("click", '[href="#"]', function(e) {
+      e.preventDefault();
+    })
+    .on("click", 'a[href^="#section"]', function(e) {
+      // section scroll
+      var el = $(this).attr("href");
+      scrollToSection($(el));
+      $("body").removeClass("is-fixed");
+      $("html").removeClass("is-fixed");
+      $(".header__list").removeClass("is-active");
+      $("[js-hamburger]").removeClass("is-active");
+      return false;
+    });
+
+  function scrollToSection(el) {
+    var headerHeight = $(".header").height();
+    var targetScroll = el.offset().top - headerHeight / 100;
+    // document.scrollingElement || document.documentElement
+
+    TweenLite.to(window, 1, {
+      scrollTo: { y: targetScroll, autoKill: false },
+      ease: easingSwing
+    });
+  }
+});
